17a85d9234b57f2dfd2a929928be92cc5c0c072e
Andrey Zadachin <zadachin86@gmail.com>
2021-11-25 06:25:40 +0300

    fixes bug

--- src/components/ProfileFoto.js
+++ src/components/ProfileFoto.js
@@ -3,7 +3,9 @@ import './ProfileFoto.css';
 const ProfileFoto = (props) => {
   return (
     <div className="profile__foto">
-      <img className="profile__foto-edit" alt="" src={props.edit_foto} />
+      <a className="link" href="#top">
+        <img className="profile__foto-edit" alt="" src={props.edit_foto} />
+      </a>
       <img className="profile__foto-img" alt="Ваше фото" src={props.profile_foto} />
     </div>
   );
